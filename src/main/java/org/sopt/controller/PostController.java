7a33a847e521a3da41729ccc166f877d5d39d4df
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-23 03:11:27 +0900

    지금까지의 작업 저장

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -3,35 +3,46 @@ package org.sopt.controller;
 import java.util.List;
 
 import org.sopt.domain.Post;
+import org.sopt.dto.PostRequest;
 import org.sopt.exception.DuplicateTitleException;
 import org.sopt.exception.TImeLimitException;
 import org.sopt.global.CheckTime;
+import org.sopt.repository.PostRepository;
 import org.sopt.service.PostService;
 import org.sopt.utils.EmojiUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PostController {
 
-	private final PostService postService = new PostService();
+	private final PostService postService;
 
-	public void createPost(String title) {
-		if(title.isEmpty()){
+	public PostController(PostService postService) {
+		this.postService = postService;
+	}
+
+	@PostMapping("/post")
+	public void createPost(@RequestBody final PostRequest postRequest) {
+		if(postRequest.getTitle().isEmpty()){
 			throw new IllegalArgumentException("제목이 비어있습니다.");
 		}else{
-			int titleLength = EmojiUtil.getEmojiLength(title);
+			int titleLength = EmojiUtil.getEmojiLength(postRequest.getTitle());
 			if(titleLength > 30){
 				throw new IllegalArgumentException("제목의 최대 길이는 30자입니다.");
 			}else if(!CheckTime.checkTime()){
 				throw new TImeLimitException();
 			}
 		}
-
-		Post post = new Post(title);
+		Post post = new Post(postRequest.getTitle());
 		postService.createPost(post);
-
 	}
-
-	public List<Post> getAllPosts(){
-		return postService.getAllPosts();
+	@GetMapping("/posts")
+	public ResponseEntity<?> getAllPosts(){
+		return ResponseEntity.ok(postService.getAllPosts());
 	}
 
 	public Post getPostById(int id) {
