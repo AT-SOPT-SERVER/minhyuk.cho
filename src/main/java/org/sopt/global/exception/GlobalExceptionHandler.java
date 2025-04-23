c7485b2de10d19faf79ef6fd7681abc407b03711
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-23 17:38:48 +0900

    feat : 게시글을 찾을 수 없을 때에 대한 에러 처리 #6

--- src/main/java/org/sopt/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/global/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package org.sopt.global.exception;
 
 import org.sopt.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,5 +21,12 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<?> handleIllegalArgument(IllegalArgumentException e){
 		return ResponseEntity.badRequest().body(
 			new ResponseDTO<>(400,e.getMessage(),null));
+	}
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<?> handleNotFoundException(EntityNotFoundException e){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+			new ResponseDTO<>(404,e.getMessage(),null)
+		);
 	}
 }
