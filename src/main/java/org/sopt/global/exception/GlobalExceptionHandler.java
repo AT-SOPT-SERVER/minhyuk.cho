0cfcfb30983d7d9d15fe3609a0ec0820a41112a7
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-23 17:18:12 +0900

    feat : GlobalExceptionHandler 기능 추가 #6

--- src/main/java/org/sopt/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/global/exception/GlobalExceptionHandler.java
@@ -1,4 +1,22 @@
 package org.sopt.global.exception;
 
+import org.sopt.dto.ResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+	@ExceptionHandler(DuplicateTitleException.class)
+	public ResponseEntity<?> handleDuplicateTitle(DuplicateTitleException e) {
+		return ResponseEntity.badRequest().body(
+			new ResponseDTO<>(400, e.getMessage(), null));
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<?> handleIllegalArgument(IllegalArgumentException e){
+		return ResponseEntity.badRequest().body(
+			new ResponseDTO<>(400,e.getMessage(),null));
+	}
 }
