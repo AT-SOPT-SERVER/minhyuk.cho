876c68bf1989a89ca3cb8eba8564f762fc83e398
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-24 13:53:49 +0900

    fix : record로 변경 #6

--- src/main/java/org/sopt/dto/PostDTO.java
+++ src/main/java/org/sopt/dto/PostDTO.java
@@ -1,21 +1,4 @@
 package org.sopt.dto;
 
-import org.sopt.domain.Post;
-
-public class PostDTO {
-	private Long contentId;
-	private String title;
-
-	public PostDTO(Post post){
-		this.contentId = post.getId();
-		this.title = post.getTitle();
-	}
-
-	public Long getContentId() {
-		return contentId;
-	}
-
-	public String getTitle() {
-		return title;
-	}
+public record PostDTO (Long contentId, String title) {
 }
--- src/main/java/org/sopt/dto/PostResponseDTO.java
+++ src/main/java/org/sopt/dto/PostResponseDTO.java
@@ -1,13 +1,4 @@
 package org.sopt.dto;
 
-public class PostResponseDTO {
-	private Long ContentId;
-
-	public PostResponseDTO(Long contentId){
-		this.ContentId = contentId;
-	}
-
-	public Long getContentId() {
-		return ContentId;
-	}
+public record PostResponseDTO(Long ContentId) {
 }
