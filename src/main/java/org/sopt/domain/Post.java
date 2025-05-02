54fd4e3da20e7b435bfa644b92e5311f5e3d1f27
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-05-02 15:26:11 +0900

    fix : user와 post에 대한 nullable 추가 #9

--- src/main/java/org/sopt/domain/Post.java
+++ src/main/java/org/sopt/domain/Post.java
@@ -1,28 +1,49 @@
 package org.sopt.domain;
 
+import org.springframework.lang.NonNull;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Post {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(nullable = false)
 	private String title;
 
-	public Post() {
+	@Column(nullable = false)
+	private String content;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	protected Post() {
 
 	}
 
-	public Post(String title) {
+	public Post(String title,String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+	public Post(String title){
 		this.title = title;
 	}
 
 	public Long getId() {
 		return id;
 	}
+
 
 	public String getTitle() {
 		return this.title;
---
+++ src/main/java/org/sopt/domain/User.java
@@ -0,0 +1,37 @@
+package org.sopt.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	private String email;
+
+	@OneToMany(mappedBy = "user")
+	private List<Post> contents = new ArrayList<>();
+
+
+	public User(){
+
+	}
+
+	public User(Long id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+}
